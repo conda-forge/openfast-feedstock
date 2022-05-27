77cf91210d12d51610f4bbd644b40d9a01826a3d
Rafael M Mudafort <rafmudaf@gmail.com>
2022-05-27 16:05:29 -0500

    Add source and build dir to CMake command

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,12 @@ mkdir build
 cd build
 
 cmake \
+    -S ${SRC_DIR} \
+    -B . \
     -DDOUBLE_PRECISION=OFF \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DBUILD_OPENFAST_CPP_API=ON
     -DBUILD_FASTFARM=ON
-    ..
 
 make -j"${CPU_COUNT}" install
