51dfc25ee9c32671b9c701e997250493da1f24ee
Rafael M Mudafort <rafmudaf@gmail.com>
2022-05-27 15:03:55 -0500

    Add more targets to the build

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,8 @@ cmake \
     -DDOUBLE_PRECISION=OFF \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=lib \
+    -DBUILD_OPENFAST_CPP_API=ON
+    -DBUILD_FASTFARM=ON
     ..
 
 make -j"${CPU_COUNT}" install
