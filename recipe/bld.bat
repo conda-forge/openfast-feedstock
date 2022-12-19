21ac43764bc68f37c9efdeb06f31aab53b746f4f
Derek Slaughter <deslaughter@gmail.com>
2022-12-19 14:59:39 -0500

    Added build.bat

---
+++ recipe/bld.bat
@@ -0,0 +1,17 @@
+mkdir build
+cd build
+
+cmake -G "Unix Makefiles" ^
+    -S %SRC_DIR% ^
+    -B . ^
+    -DDOUBLE_PRECISION=OFF ^
+    -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+    -DCMAKE_INSTALL_LIBDIR=lib ^
+    -DBUILD_FASTFARM=ON ^
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    
+if errorlevel 1 exit 1
+
+cmake --build . --target install
+
+if errorlevel 1 exit 1
