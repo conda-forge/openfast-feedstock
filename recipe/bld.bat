d30700fb5618e7fba3a2122f26577fde9f515a2e
Derek Slaughter <deslaughter@gmail.com>
2023-11-29 18:52:04 -0500

    Updating windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,7 +9,6 @@ cmake ^
     -S %SRC_DIR% ^
     -B . ^
     -G "MinGW Makefiles" ^
-    -DCMAKE_MAKE_PROGRAM="C:/ProgramData/chocolatey/bin/make.exe" ^
     -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" ^
     -DCMAKE_BUILD_TYPE="Debug" ^
     -DCMAKE_C_FLAGS="-static" ^
@@ -19,17 +18,17 @@ cmake ^
     -DDOUBLE_PRECISION=OFF ^
     -DBLA_VENDOR=OpenBLAS ^
     -DBLA_STATIC=ON ^
-    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_LIBDIR=lib ^
     -DBUILD_FASTFARM=ON ^
-    -DCMAKE_C_COMPILER="C:/ProgramData/chocolatey/bin/gcc.exe" ^
-    -DCMAKE_CXX_COMPILER="C:/ProgramData/chocolatey/bin/g++.exe" ^
-    -DCMAKE_Fortran_COMPILER="C:/ProgramData/chocolatey/bin/gfortran.exe"
+    -DCMAKE_C_COMPILER=gcc.exe ^
+    -DCMAKE_CXX_COMPILER=g++.exe ^
+    -DCMAKE_Fortran_COMPILER=gfortran.exe
 
 if errorlevel 1 exit /b 1
 	
-cmake --build . -j 1
+cmake --build . -j 2
 if errorlevel 1 exit /b 1
 
 cmake --install .
