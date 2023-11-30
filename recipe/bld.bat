245a72809cca25e4402004ccda1ee3806a7e6369
Derek Slaughter <deslaughter@gmail.com>
2023-11-29 21:25:12 -0500

    Resolving Windows dlls

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,18 +9,11 @@ cmake ^
     -S %SRC_DIR% ^
     -B . ^
     -G "MinGW Makefiles" ^
-    -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" ^
-    -DCMAKE_BUILD_TYPE="Debug" ^
-    -DCMAKE_C_FLAGS="-static" ^
-    -DCMAKE_CXX_FLAGS="-static" ^
-    -DCMAKE_Fortran_FLAGS="-static" ^
-    -DBUILD_SHARED_LIBS=OFF ^
     -DDOUBLE_PRECISION=OFF ^
     -DBLA_VENDOR=OpenBLAS ^
     -DBLA_STATIC=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -DCMAKE_INSTALL_LIBDIR=lib ^
     -DBUILD_FASTFARM=ON ^
     -DCMAKE_C_COMPILER=gcc.exe ^
     -DCMAKE_CXX_COMPILER=g++.exe ^
@@ -32,4 +25,7 @@ cmake --build . -j 2
 if errorlevel 1 exit /b 1
 
 cmake --install .
+if errorlevel 1 exit /b 1
+
+del %LIBRARY_PREFIX%\include %LIBRARY_PREFIX%\lib
 if errorlevel 1 exit /b 1
