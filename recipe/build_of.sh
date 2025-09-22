0e1181ad19a128fb3733fae3b9121c093cb275ec
Garrett Barter <garrett.barter@nrel.gov>
2025-09-22 11:38:54 -0600

    reducing build times

--- recipe/build_of.sh
+++ recipe/build_of.sh
@@ -2,16 +2,25 @@
 
 mkdir build
 
+if [ "$(uname)" == "Darwin" ]; then
+    btype="RelWithDebInfo"
+    vtrack=0
+else
+    btype="Release"
+    vtrack=1
+fi
+
 cmake \
     -S ${SRC_DIR} \
     -B build \
     -G "Ninja" \
-    -DCMAKE_BUILD_TYPE="Release" \
+    -DCMAKE_BUILD_TYPE=${btype} \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DGIT_DESCRIBE=v${PKG_VERSION} \
     -DDOUBLE_PRECISION=OFF \
     -DBLA_VENDOR=OpenBLAS \
     -DBLA_STATIC=ON \
-    -DBUILD_FASTFARM=ON
+    -DBUILD_FASTFARM=ON \
+    -DVARIABLE_TRACKING=${vtrack}
 
 cmake --build build --target install -j ${CPU_COUNT}
